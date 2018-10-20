e9fb3c57fe8814b3e44c1acff3bc91a78122b8c3
BuildTools <unconfigured@null.spigotmc.org>
2018-10-20 12:49:19 +0200

    buttons styling + opening drawer on burger_icon tap

--- app/src/main/java/com/gdziejestmecz/gdzie_jest_mecz/MainScreen.java
+++ app/src/main/java/com/gdziejestmecz/gdzie_jest_mecz/MainScreen.java
@@ -1,14 +1,17 @@
 package com.gdziejestmecz.gdzie_jest_mecz;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,8 +39,10 @@ public class MainScreen extends AppCompatActivity implements GoogleApiClient.OnC
     private ImageView userAvatarImageView;
     private GoogleApiClient googleApiClient;
 
+    private DrawerLayout drawerLayout;
     private NavigationView sideBar;
     private Menu sideMenu;
+    private Button plusBtn, menuBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,16 +58,29 @@ public class MainScreen extends AppCompatActivity implements GoogleApiClient.OnC
     }
 
     private void addEventListeners() {
-
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainScreen.this, "@string/plus_tapped", Toast.LENGTH_SHORT).show();
+            }
+        });
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainScreen.this, "@string/menu_tapped", Toast.LENGTH_SHORT).show();
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 
     private void initUIElements() {
+        this.drawerLayout = findViewById(R.id.drawerLayout);
         this.sideBar = findViewById(R.id.sideNav);
+        this.plusBtn = findViewById(R.id.plus_btn);
+        this.menuBtn = findViewById(R.id.menuBtn);
 
         View headerLayout = sideBar.getHeaderView(0);
-        this.userFirstnameLabel = (TextView) headerLayout.findViewById(R.id.userFirstnameLabel);
-        this.userEmailLabel = (TextView) headerLayout.findViewById(R.id.userEmailLabel);
-        this.userAvatarImageView= (ImageView) headerLayout.findViewById(R.id.userAvatarImageView);
+        this.userFirstnameLabel = headerLayout.findViewById(R.id.userFirstnameLabel);
+        this.userEmailLabel = headerLayout.findViewById(R.id.userEmailLabel);
+        this.userAvatarImageView = headerLayout.findViewById(R.id.userAvatarImageView);
 
     }
 
@@ -122,7 +140,6 @@ public class MainScreen extends AppCompatActivity implements GoogleApiClient.OnC
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
         switch (item.getItemId()) {
             case R.id.sign_out_label:
                 signOut();
