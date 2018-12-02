f8d566aaaa4a1792b5360ac8c59e4597e14794d2
flown1 <Prometeusz1>
2018-12-02 19:46:59 +0100

    xml rows

--- app/src/main/java/com/gdziejestmecz/gdzie_jest_mecz/components/mainScreen/PubSpinnerListAdapter.java
+++ app/src/main/java/com/gdziejestmecz/gdzie_jest_mecz/components/mainScreen/PubSpinnerListAdapter.java
@@ -1,41 +1,50 @@
 package com.gdziejestmecz.gdzie_jest_mecz.components.mainScreen;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.gdziejestmecz.gdzie_jest_mecz.R;
 import com.gdziejestmecz.gdzie_jest_mecz.models.Match;
 import com.gdziejestmecz.gdzie_jest_mecz.models.Pub;
 
 import java.util.ArrayList;
 
+
 public class PubSpinnerListAdapter extends ArrayAdapter<Pub> {
     private Context context;
     private ArrayList<Pub> pubList;
+    private LayoutInflater inflater;
 
     public PubSpinnerListAdapter(Context context, ArrayList<Pub> data) {
         super(context, -1, -1, data); //dubel id?
         this.context = context;
         this.pubList = data;
+
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    private View getCustomView(int position, View convertView, ViewGroup parent) {
+        Pub pub = super.getItem(position);
+
+        RelativeLayout pubSpinnerRow = (RelativeLayout) inflater.inflate(R.layout.pubs_spinner_row, null, false);
+        TextView pubName = (TextView) pubSpinnerRow.findViewById(R.id.pub_name_spinner_row);
+
+        pubName.setText(pub.getName());
+        return pubSpinnerRow;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-        Pub pub = super.getItem(position);
-        TextView test = new TextView(context);
-        test.setText(pub.getName());
-
-        return test;
+        return getCustomView(position, convertView, parent);
     }
 
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent) {
-        TextView label = new TextView(context);
-        Pub pub = super.getItem(position);
-        label.setText(pub.getName());
-
-        return label;
+        return getCustomView(position, convertView, parent);
     }
 }
